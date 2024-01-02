a1867ca0906d0642241c9fb07a8ce84ff28d291f
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2024-01-03 00:09:14 +0700

    create navbar part-1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1 className=" text-2xl font-bold">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
